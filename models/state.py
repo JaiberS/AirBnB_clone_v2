565d7aaacd743b81fce2b0109b2f73f77b084444
marianaplazas <marianaplazas@live.com>
2019-08-14 21:57:27 +0000

    add getter

--- models/state.py
+++ models/state.py
@@ -5,6 +5,8 @@ from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import environ
+import models
+
 
 if "HBNB_TYPE_STORAGE" in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
     class State(BaseModel, Base):
@@ -20,3 +22,13 @@ else:
         this is the state class
         """
         name = ""
+
+        @property
+        def cities(self):
+            all_cities = models.storage.all()
+            lista = []
+            keys = all_cities.items()
+            for i, j in keys:
+                if "City" == i[0:4] and j.state_id == self.id:
+                    lista.append(j)
+            return(lista)
