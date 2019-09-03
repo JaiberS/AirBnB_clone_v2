ea0f73ef45b440a207627e8f8d29620c90fb2d32
JaiberS <758@holbertonschool.com>
2019-09-03 23:56:41 +0000

    [Feature] if on html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Flask """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ string to be returned """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ string to be returned """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """ string to be returned """
+    return 'C {}'.format(text).replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text='is cool'):
+    """ string to be returned """
+    return 'Python {}'.format(text).replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_number(n):
+    """ string to be returned """
+    return '{} is a number'.format(n).replace('_', ' ')
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def hello_template(n):
+    """ string to be returned """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def hello_odd_or_even(n):
+    """ string to be returned """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
